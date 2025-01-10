bde4bbb32f014f8204b394e9243501eb09b7e599
sabarish-p-wailatech <117067397+GuhanUK@users.noreply.github.com>
2025-01-10 14:44:29 +0530

    main
    completed

--- src/Routes/RoutesData.js
+++ src/Routes/RoutesData.js
@@ -1,5 +1,6 @@
 import DataView from "../Components/Pages/DataView";
 import Login from "../Components/Pages/Login";
+import PermissionManager from "../summa";
 
 const unProtectedRoutes = [
   {
@@ -16,6 +17,11 @@ const protectedRoutes = [
   {
     path: "/",
     component: <DataView />,
+    key: "dashboard",
+  },
+  {
+    path: "/summa",
+    component: <PermissionManager />,
     key: "dashboard",
   },
 ];
